7157ad4c286e2bae7f478dd6bcb745c0482012d7
flipmotion <flipmotions@gmail.com>
2015-06-26 17:44:41 +0300

    last
    last

--- build/js/my/init.js
+++ build/js/my/init.js
@@ -5,6 +5,38 @@ $(document).ready(function() {
 		}, 1000);
 		return false;
 	});
+	/*$('.category-type .my-item').click(function(e){
+		var that = $('.category-type .my-item a');
+		var target = $(e.relatedTarget);
+		if(target != that) {
+			alert(false);
+		}
+		else {
+			alert(true);
+		}
+	});*/
+	var to = $('[data-item="target"] img');
+	var from = $('[data-item="slider-sync-1"] img');
+	
+	from.click(function() {
+		var loc = $(this).attr("src");
+		to.attr("src",loc);
+	});
+	
+	var sync1 = $('[data-item="slider-sync-1"]');
+	
+	sync1.owlCarousel({
+		loop : true,
+		items : 3,
+		margin:0,
+		nav : true,
+		thumbs: true,
+		margin: 10,
+		navText: [
+			"<i class='my-arrow-left'></i>",
+			"<i class='my-arrow-right'></i>"
+		]
+	});
 	
 	var owlMain = $('[data-item="slider-main"]');
 	owlMain.owlCarousel({
@@ -19,7 +51,6 @@ $(document).ready(function() {
 		navText: [
 			"<i class='my-arrow-left'></i>",
 			"<i class='my-arrow-right'></i>"
-		],
-		dots: true,
+		]
 	});
 });
